0199823d5b411c342a9a8263d2b4a17d941f8731
UnicornBear <jtbregenzer@gmail.com>
2019-05-13 22:06:13 -0600

    concert this function

--- liri.js
+++ liri.js
@@ -8,16 +8,19 @@
 
     var keys = require("./keys.js");
 
+    // need moment package for the concert-this function
+    var moment = require("moment");
 
 // switch function for the desired actions [2]
 // movie-this  |  do-what-it-says  |  spotify-this-song  |  concert-this
 switch (action) {
-	case "concert-this":
-	concert(userInput);
-	break;
 
 	case "spotify-this-song":
 	spotify(userInput);
+    break;
+    
+    case "concert-this":
+	concert(userInput);
 	break;
 
 	case "movie-this":
@@ -29,7 +32,7 @@ switch (action) {
 	break;
 };
 
-// spotify-this-song
+// spotify-this-song function
 function spotify(userInput) {
 
 	var spotify = new Spotify(keys.spotify);
@@ -48,4 +51,25 @@ function spotify(userInput) {
 	        console.log("Album: " + songs[0].album.name);
           console.log('=========================================');
 	});
+};
+
+//concert-this function
+function concert(userInput) {
+    // queryURL for the Town Artist Events API
+    var queryURL = "https://rest.bandsintown.com/artists/" + userInput + "/events?app_id=codingbootcamp";
+
+    // data request for queryURL
+    request(queryURL, function(error,response,data){
+        // json parse body
+        var jsonData = JSON.parse(data);
+          for(var i in jsonData) {
+           temp = jsonData[i].datetime;
+           console.log('Artist(s):' + process.argv[3]);
+           console.log('Venue Name:' + jsonData[i].venue.name);
+           console.log('date: ' + moment(temp).format('DD/MM/YYYY'));
+           console.log('City/Country: ' + jsonData[i].venue.city + ', ' + jsonData[i].venue.country)
+           console.log('Location : ' + jsonData[i].venue.latitude + ', ' + jsonData[i].venue.longitude);
+           console.log('=========================================');
+        }
+    });
 };
