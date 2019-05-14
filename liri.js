844259b72d445d4e25f574c52dca44f7f77b7ce4
UnicornBear <jtbregenzer@gmail.com>
2019-05-13 19:58:09 -0600

    start of code - stop - dad stop on by

--- liri.js
+++ liri.js
@@ -1,5 +1,16 @@
+
+// info for Spotify API
+
 require("dotenv").config();
 
-var keys = require("./keys.js");
+    var Spotify = require("node-spotify-api");
+    var keys = require("./keys.js");
+    var spotify = new Spotify(keys.spotify);
 
-var spotify = new Spotify(keys.spotify);
+ 
+// request return
+    var request = require("request");
+
+// action request ( )
+var action = process.argv[2];
+
