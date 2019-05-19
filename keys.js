08de187fc0a1a2dc8ea1f38ff84bc642c562abd8
UnicornBear <46576621+UnicornBear@users.noreply.github.com>
2019-05-19 15:58:53 -0600

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,14 +1,5 @@
-<<<<<<< HEAD
 console.log('this is loaded');
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
-=======
-console.log('this is loaded');
-
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
->>>>>>> aae00b9fb8860ad078a5b601d446b950c7fbb409
-};
